b47b7553c40a28ef0b684d27e9a9eed902087feb
Neeraj2319 <42376511+Neeraj2319@users.noreply.github.com>
2020-05-04 19:17:02 +0530

    Create array.java

---
+++ array.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication8;
+
+/**
+ *
+ * @author jayar
+ */
+public class JavaApplication8 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        try {
+  int[] myNumbers = {1, 2, 3};
+  System.out.println(myNumbers[2]);
+} catch (Exception e) {
+  System.out.println("Something went wrong.");
+} 
+finally
+ {
+  System.out.println("The 'try catch' is finished.");
+}
+        // TODO code application logic here
+    }
+    
